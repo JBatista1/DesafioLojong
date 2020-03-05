d9c325c581e0a7dca85b5572434fb5af45bf9791
João Batista <j.batista.romao@gmail.com>
2020-03-04 21:41:58 -0300

    Ajusta Swift Lint

--- Desafio/Screens/Home/Controller/HomesViewController.swift
+++ Desafio/Screens/Home/Controller/HomesViewController.swift
@@ -7,24 +7,41 @@
 //
 
 import UIKit
-
+import MediaPlayer
+import AVKit
 class HomesViewController: UIViewController {
     let customView = Home()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        play()
+        
     }
     override func loadView() {
-        view = customView
+    }
+    
+    func play() {
+        
+        guard let videoURL: URL = URL(string: "https://d35mkqei6qg744.cloudfront.net/pt/videos/1felicidadegenuina.mp4") else {
+            return print("Deu merda em")
+        }
+            let player = AVPlayer(url: videoURL)
+            let playerViewController = AVPlayerViewController()
+            playerViewController.player = player
+            self.present(playerViewController, animated: true) {
+                playerViewController.player!.play()
+            }
+        
     }
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
